f19d5e77780df7930a4b5ecae79387c3d809023a
Srajan Jaiswal <68801705+Srajan-Jaiswal@users.noreply.github.com>
2020-09-07 14:05:11 +0530

    Create Codeforces Round #619 (Div. 2).cpp

---
+++ Codeforces Round #619 (Div. 2).cpp
@@ -0,0 +1,42 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long
+#define fr(i,a,n)  for(int i=a;i<n;i++)
+#define fre(i,a,b)  for(int i=a;i<=b;i++)
+#define endl '\n'
+#define no cout << "NO" << endl
+#define yes cout << "YES" << endl
+#define mem(name, value) memset(name, value, sizeof(name))
+#define pb push_back
+#define ff first
+#define ss second
+#define mp make_pair
+#define all(v) (v).begin(), (v).end()
+#define case cout << "Case " << t++ << ": ";
+
+int32_t main()
+{
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif
+	int t;
+	cin >> t;
+	while (t--)
+	{
+		string a, b, c;
+		cin >> a >> b >> c;
+		bool flag = 0;
+		for (int i = 0; i < a.length(); i++) {
+			if (c[i] != a[i] && c[i] != b[i])
+				flag = 1;
+		}
+		if (flag)
+			cout << "NO" << endl;
+		else
+			cout << "YES" << endl;
+	}
+}
